80c4413d7c4ea710f2b2e41e34be49d269482e91
Raymond GA Côté <ray.cote@mac.com>
2024-09-14 15:01:01 -0400

    First three line length practices

---
+++ Chapter3/streams-practice.exs
@@ -0,0 +1,48 @@
+defmodule LinesLengths do
+  @moduledoc """
+  Line lengths of a file using streams.
+  """
+
+  def lines_lengths!(path) do
+    path
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing(&1, "\n"))
+    |> Enum.map(&String.length(&1))
+  end
+
+  def longest_line_length!(path) do
+    path
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing(&1, "\n"))
+    |> Stream.map(&String.length(&1))
+    |> Enum.max()
+  end
+
+  def _acc_longest_line(line, %{line_length: acc_length, text: acc_text}) do
+    line_length = String.length(line)
+
+    if line_length > acc_length do
+      %{line_length: line_length, text: line}
+    else
+      %{line_length: acc_length, text: acc_text}
+    end
+  end
+
+  def longest_line!(path) do
+    result =
+      path
+      |> File.stream!()
+      |> Stream.map(&String.trim_trailing(&1, "\n"))
+      |> Enum.reduce(%{line_length: 0, text: ""}, fn item, acc ->
+        _acc_longest_line(item, acc)
+      end)
+
+    %{line_length: line_length, text: text} = result
+    text
+  end
+end
+
+path = "./streams-practice.exs"
+IO.inspect(LinesLengths.lines_lengths!(path), label: "Line Lengths")
+IO.inspect(LinesLengths.longest_line_length!(path), label: "Longest line length")
+IO.inspect(LinesLengths.longest_line!(path), label: "Longest line")
