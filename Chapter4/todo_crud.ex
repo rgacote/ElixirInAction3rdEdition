453fc0a05b3e1447a9bea4e7bab54eef48a96744
Raymond GA Côté <ray.cote@mac.com>
2024-09-15 11:41:52 -0400

    Chapter 4.2.4: ToDo CRUD

---
+++ Chapter4/todo_crud.ex
@@ -0,0 +1,44 @@
+defmodule TodoList do
+  defstruct next_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.next_id)
+    new_entries = Map.put(todo_list.entries, todo_list.next_id, entry)
+    %TodoList{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
+  end
+
+  def update_entry(todo_list, entry_id, updater_func) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_func.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  # Throws error on missing key.
+  def delete_entry_rgac(todo_list, entry_id) do
+    Map.delete(todo_list.entries, entry_id)
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Map.values()
+    |> Enum.filter(fn entry -> entry.date == date end)
+  end
+end
